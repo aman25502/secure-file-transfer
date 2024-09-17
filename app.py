8cb2d4f912158064e3ec8394e72166bf5726d1bc
aman25502 <65896145+aman25502@users.noreply.github.com>
2024-09-17 16:48:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,4 +90,4 @@ def send_email(receiver_email, file_path, key):
     mail.send(msg)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
